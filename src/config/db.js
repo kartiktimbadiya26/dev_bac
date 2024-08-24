00f64b1d65022270087073923b9805c4576c58ba
kartiktimbadiya26 <158551479+kartiktimbadiya26@users.noreply.github.com>
2024-08-24 13:04:16 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
-// const mongoDbUrl='mongodb+srv://codewithzosh:lognBCBmtWNPjrKC@cluster0.wvt9jpw.mongodb.net/?retryWrites=true&w=majority'
-const mongoDbUrl = "mongodb+srv://kartik:PE6HmGpXhWuLnTQk@cluster0.bej3wgt.mongodb.net/?retryWrites=true&w=majority";
+const mongoDbUrl='mongodb+srv://codewithzosh:lognBCBmtWNPjrKC@cluster0.wvt9jpw.mongodb.net/?retryWrites=true&w=majority'
+// const mongoDbUrl = "mongodb+srv://kartik:PE6HmGpXhWuLnTQk@cluster0.bej3wgt.mongodb.net/?retryWrites=true&w=majority";
 const connectDb = () => {
     return mongoose.connect(mongoDbUrl)
 }
